426f12ef85303cd16f4fd7cbe239fc8d81d2d8e2
VEIDD <separik3@gmail.com>
2025-07-22 22:25:44 +0300

    іфвц

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
 import { Layout } from './Layout'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Weather } from './components/weather/Weather'
 import { WeatherDay } from './components/weather/WeatherDay'
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/weather'>
+  <HashRouter>
     <Routes>
       <Route path='/' element={<Layout/>} />
       <Route path='/weather-day' element={<WeatherDay/>} />
       <Route path='/weather' element={<Weather/>} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 )
