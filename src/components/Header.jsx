8a7a3ca5a043289fb3adbd835c6f5e90d97af72c
VEIDD <separik3@gmail.com>
2025-07-22 22:08:35 +0300

    deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -49,7 +49,7 @@ export const Header = ({ onChangeTheme }) => {
       <div className={styles.btns}>
         <div className={styles.searchBlock}>
           <img
-            src="public/search_icon.svg"
+            src="/search_icon.svg"
             alt=""
             width={25}
             className={styles.search_icon}
@@ -79,7 +79,7 @@ export const Header = ({ onChangeTheme }) => {
             setShowSettings((prev) => !prev);
           }}
         >
-          <img src="public/settings_icon.svg" alt="" width={30} />
+          <img src="/settings_icon.svg" alt="" width={30} />
         </button>
       </div>
     </header>
